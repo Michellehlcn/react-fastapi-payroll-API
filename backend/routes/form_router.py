28d17c44fc7b5dabdd1a5097dd7f383820ea6f8f
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-04-19 07:53:36 +1000

    Add files via upload

---
+++ backend/routes/form_router.py
@@ -0,0 +1,107 @@
+import logging
+logger = logging.getLogger('foo-logger')
+
+
+from typing import List,Optional
+
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi import APIRouter,Depends,Request,responses,status
+
+import fastapi as _fastapi
+from fastapi.templating import Jinja2Templates
+import sqlalchemy.orm as _orm
+import database as _database
+
+import models.user_model as user_model
+import schemas.user_schema as user_schema
+import routes.user_router as user_router
+import models.form_model as form_model
+import schemas.form_schema as form_schema
+import services.form_service as form_service
+
+
+
+#....
+
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+    
+@router.get("/")
+async def home(request: Request, db: _orm.Session = _fastapi.Depends(_database.get_db), msg: str = None):
+    #jobs = list_jobs(db=db)
+    return templates.TemplateResponse(
+        "homepage.html", {"request": request, "msg": msg}
+    )
+
+@router.get("/details/{id}")
+def timesheet_detail(id: int, request: _fastapi.Request, db:_orm.Session = _fastapi.Depends(_database.get_db)):
+    job = form_service.FormService.retreive_job(id=id, db=db)
+    return templates.TemplateResponse(
+        "detail_timesheet.html", {"request": request, "job": job}
+    )
+
+@router.get("/my-timesheets", response_model=List[form_schema.ShowJob])
+async def get_user_jobs(request: _fastapi.Request,
+                         db: _orm.Session = _fastapi.Depends(_database.get_db)):
+    token = request.cookies.get("access_token")
+    scheme, param = get_authorization_scheme_param(
+        token
+    )  # scheme will hold "Bearer" and param will hold actual token value
+    current_user:  user_schema.User=user_router.get_current_user_from_token(db=db,token=param)
+    return form_service.FormService.get_user_jobs(user=current_user, db=db)
+
+
+@router.get("/post-a-timesheet/",response_model=form_schema.ShowJob)       
+def create_job(request: Request, db: _orm.Session = _fastapi.Depends(_database.get_db)):
+    return templates.TemplateResponse("form_timesheet.html", {"request": request})
+
+
+@router.post("/post-a-timesheet/",response_model=form_schema.ShowJob)   
+async def create_job(
+       request: Request, 
+       db: _orm.Session = _fastapi.Depends(_database.get_db)):
+       #user: user_schema.User = _fastapi.Depends(user_router.get_current_user)):
+
+    form = form_model.JobCreateForm(request)
+    await form.load_data()
+    if form.is_valid():
+        try:
+            token = request.cookies.get("access_token")
+            scheme, param = get_authorization_scheme_param(
+                token
+            )  # scheme will hold "Bearer" and param will hold actual token value
+            current_user:  user_schema.User=user_router.get_current_user_from_token(db=db,token=param)
+            job = form_schema.JobCreate(**form.__dict__)
+
+            job = form_service.FormService.create_new_job(job=job, db=db, user=current_user)
+            print(current_user)
+            return responses.RedirectResponse(
+                f"/form/details/{job.id}", status_code=status.HTTP_302_FOUND
+            )
+        except Exception as e:
+            print(e)
+            form.__dict__.get("errors").append(
+                "You might not be logged in, In case problem persists please contact us."
+            )
+            return templates.TemplateResponse("form_timesheet.html", form.__dict__)
+        #logger.debug('This is test')
+    return templates.TemplateResponse("form_timesheet.html", form.__dict__)
+
+
+@router.get("/delete-timesheet/")
+def show_jobs_to_delete(request: _fastapi.Request, db:_orm.Session = _fastapi.Depends(_database.get_db)):
+    jobs = form_service.FormService.list_jobs(db=db)
+    return templates.TemplateResponse(
+        "delete_timesheet.html", {"request": request, "jobs": jobs}
+    )
+
+
+@router.get("/search/")
+def search(
+    request: _fastapi.Request, db:_orm.Session = _fastapi.Depends(_database.get_db), query: Optional[str] = None
+):
+    jobs = form_service.FormService.search_job(query, db=db)
+    return templates.TemplateResponse(
+        "homepage.html", {"request": request, "jobs": jobs}
+    )
---
+++ backend/routes/user_router.py
@@ -0,0 +1,188 @@
+from fastapi import APIRouter,Depends,Request,responses,status
+from typing import List, Optional
+from datetime import datetime, timedelta
+import os as _os
+
+import dotenv as _dotenv
+import jwt as _jwt
+from jose import JWTError
+import sqlalchemy.orm as _orm
+from passlib.context import CryptContext
+
+import fastapi as _fastapi
+import fastapi.security as _security
+
+import database as _database
+import schemas.user_schema as _schemas
+import models.user_model as _models
+from services.user_service import UserService, OAuth2PasswordBearerWithCookie
+import pydantic as _pydantic
+
+router = APIRouter()
+
+_dotenv.load_dotenv()
+# JWT CONFIGURATIONS
+#_JWT_SECRET = _os.environ['JWT_SECRET']
+_JWT_SECRET = 'password123'
+oauth2schema = _security.OAuth2PasswordBearer("users/token")
+ACCESS_TOKEN_EXPIRE_MINUTES = 90
+
+
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/users/token")
+
+class Token(_pydantic.BaseModel):
+    access_token: str
+    token_type: str
+class TokenData(_pydantic.BaseModel):
+    email: Optional[str] = None
+
+
+
+
+
+
+
+
+# create access token
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode.update({"exp": expire})
+    encoded_jwt = _jwt.encode(
+        to_encode, _JWT_SECRET, "HS256"
+    )
+    return encoded_jwt
+
+
+def create_token(user: _models.User):
+    user_obj = _schemas.User.from_orm(user)
+
+    user_dict = user_obj.dict()
+    del user_dict["date_created"]
+
+    token = _jwt.encode(user_dict, _JWT_SECRET)
+    #return token
+
+    return dict(access_token=token, token_type="bearer")
+
+
+# create get user dependency
+def get_current_user(db: _orm.Session = _fastapi.Depends(_database.get_db), token: str = _fastapi.Depends(oauth2schema)):
+
+    try:
+        payload = _jwt.decode(token, _JWT_SECRET, algorithms=["HS256"])
+        user = db.query(_models.User).get(payload["id"])
+    
+    except:
+        raise _fastapi.HTTPException(
+            status_code=401, detail="Invalid Email or Password"
+        )
+    print(_schemas.User.from_orm(user))
+    return _schemas.User.from_orm(user)
+
+
+#Register
+@router.post("")
+async def create_user(user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_database.get_db)):
+    db_user = UserService.get_user_by_email(email=user.email, db=db)
+    if db_user:
+        raise _fastapi.HTTPException(
+            status_code=400,
+            detail="Email already in use")
+    user = UserService.create_user(user=user, db=db)
+    return create_token(user=user)
+
+'''
+#login
+@router.post("/token",response_model=Token)
+async def generate_token(
+    response: _fastapi.Response,  
+    form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(), 
+    db: _orm.Session = _fastapi.Depends(_database.get_db)):
+    user = authenticate_user(email=form_data.username, password=form_data.password, db=db)
+
+    if not user:
+        raise _fastapi.HTTPException(
+            status_code=401, detail="Incorrect email or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires)
+    response.set_cookie(
+        key="access_token", value=f"Bearer {access_token}", httponly=True
+    )
+    #return create_token(user=user)
+    return {"access_token": access_token, "token_type": "bearer"}'''
+
+
+def get_current_user_from_token(
+    token: str = _fastapi.Depends(oauth2_scheme), db: _orm.Session = _fastapi.Depends(_database.get_db)
+):
+    credentials_exception = _fastapi.HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = _jwt.decode(
+            token, _JWT_SECRET, algorithms=["HS256"]
+        )
+        email: str = payload.get("sub")
+        print("email extracted is ", email)
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = UserService.get_user(email=email)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@router.post("/token")
+async def generate_token(
+    form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(),
+    db: _orm.Session = _fastapi.Depends(_database.get_db),
+):
+    user = UserService.authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
+
+    return create_token(user)
+
+@router.get("/me", response_model=_schemas.User)
+async def get_user(user: _schemas.User = _fastapi.Depends(get_current_user)):
+    return user
+
+
+
+
+
+#Super user role
+class RoleChecker:
+    def __init__(self, allowed_roles: List):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, user: _schemas.User = _fastapi.Depends(get_current_user)):
+        if user.role not in self.allowed_roles:
+            logger.debug(f"User with role {user.role} not in {self.allowed_roles}")
+            raise HTTPException(status_code=403, detail="Operation not permitted")
+
+#Super user role
+allow_create_resource = RoleChecker(["admin","manager"])
+
+
+def verify_role(required_role: List, user: _schemas.User = _fastapi.Depends(get_current_user)):
+    if user.role not in required_role:
+        raise HTTPException(status_code=403, detail="Operation not permitted")
+
+
+
+
+
+
+
