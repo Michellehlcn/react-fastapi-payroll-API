f4c34c2a13d8cd00d65dd095496e6581a12cc2df
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-04-22 16:38:37 +1000

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -15,7 +15,7 @@ export const UserProvider = (props) => {
         },
       };
 
-      const response = await fetch("/users/me", requestOptions);
+      const response = await fetch("api/users/me", requestOptions);
 
       if (!response.ok) {
         setToken(null);
