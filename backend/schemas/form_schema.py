473c0b421dde1e95463b21d52b3558c0b79bed60
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-04-22 14:12:56 +1000

    Update form_schema.py

--- backend/schemas/form_schema.py
+++ backend/schemas/form_schema.py
@@ -6,11 +6,22 @@ import pydantic as _pydantic
 
 # shared properties
 class JobBase(_pydantic.BaseModel):
-    title: Optional[str] = None
-    time: Optional[str] = None
     subject: Optional[str] = None
-    location: Optional[str] = "Remote"
-    description: Optional[str] = None
+    campus: Optional[str] = "Remote"
+    day: Optional[str] = None
+    am_pm_eve: Optional[str] = None
+    time: Optional[str] = None
+    course: Optional[str] = None
+    group: Optional[str] = None
+    zoom_id_for_trainer: Optional[str] = None
+    zoom_password_for_trainer: Optional[str] = None
+    zoom_link_for_students: Optional[str] = None
+    campus_room_no_capacity: Optional[str] = None
+    classrom_capacity: Optional[int] = None
+    number_of_student: Optional[int] = None
+    student_profile: Optional[str] = None
+    class_size_utilization: Optional[str] = None
+    unique_group: Optional[str] = None
 
 # this will be used to validate data while creating a Job
 class JobCreate(JobBase):
@@ -21,6 +32,7 @@ class ShowJob(JobBase):
     id: int
     owner_id: int
     date_created: _dt.datetime
+    is_active: bool
 
     class Config:  # to convert non dict obj to json
         orm_mode = True
