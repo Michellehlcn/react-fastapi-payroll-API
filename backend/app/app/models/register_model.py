36a8cd70018134a57a962c99fbb1f841e411083c
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-05-01 14:02:48 +1000

    Add files via upload

---
+++ backend/app/app/models/register_model.py
@@ -0,0 +1,26 @@
+from typing import List
+from typing import Optional
+
+from fastapi import Request
+
+
+class UserCreateForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.errors: List = []
+        self.email: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def load_data(self):
+        form = await self.request.form()
+        self.email = form.get("email")
+        self.password = form.get("password")
+
+    async def is_valid(self):
+        if not self.email or not (self.email.__contains__("@")):
+            self.errors.append("Email is required")
+        if not self.password or not len(self.password) >= 4:
+            self.errors.append("Password must be > 4 chars")
+        if not self.errors:
+            return True
+        return False
