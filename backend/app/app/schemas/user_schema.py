399a9d8daf83a2a133e9d4cd122fb2d7caef101d
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2022-05-01 13:59:30 +1000

    Add files via upload

---
+++ backend/app/app/schemas/user_schema.py
@@ -0,0 +1,38 @@
+
+import datetime as _dt
+
+import pydantic as _pydantic
+
+class _UserBase(_pydantic.BaseModel):
+    email: str
+    role: str
+
+class UserCreate(_UserBase):
+    password: str
+
+
+    class Config:
+        orm_mode = True
+
+
+class User(_UserBase):
+    id: int
+    date_created: _dt.datetime
+    active: bool
+
+    class Config:
+        orm_mode = True
+
+class _PostBase(_pydantic.BaseModel):
+    post_text: str
+
+class PostCreate(_PostBase):
+    pass
+
+class Post(_PostBase):
+    id: int
+    owner_id: int
+    date_created: _dt.datetime
+
+    class Config:
+        orm_mode = True
